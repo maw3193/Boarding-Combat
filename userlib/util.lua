d9592369d0be92881ff70d696c905938916b25e5
Jonathan Maw <jonathan.maw@gmail.com>
2012-03-11 19:03:14 +0000

    Text boxes store lines of text properly (?), only print what they can fit in the area, and can be scrolled with mousewheel when hovering over the textbox

--- userlib/util.lua
+++ userlib/util.lua
@@ -18,47 +18,80 @@ function util.addbitmap(path)
 end
 
 function util.getlinewithspaces(str, width, start)
-	local endpos
-	local string = nil
-	local curr
-	local pos
-	local nextstart
-	local addnewline = false
-	curr = start
-	while (true) do
-		pos, _ = str:find("\n", curr)
-		if (pos ~= nil) then --a newline exists
-			if (pos - start) < width then -- the newline was within
-				endpos = pos + 1      -- width
-				break
-			end
-		end
+--[[
+Gets a line of maximum length <width>, returns the line and the next index.
+RULES:	Check if we're at the end of the string. DONE
+	Check if the end is less than <width> away from <start> DONE
+	Check if there is a new line within range. DONE
+	Check if there are any spaces in range.
+		If none, get <width> characters as the line.
+		If some, loop through to get the last space before the limit.
 	
-		pos, _ = str:find("%s", curr)
-		if (pos ~= nil) then --have not reached the end of the string
-			if (pos - start) < width then
-				curr = pos + 1
-			else
-				endpos = curr
-				addnewline = true
-				break
+--]]
+	local limit = width + start - 1
+	local pos
+	local string
+	local nextchar
+	if (start >= #str) then --String is at or beyond the end
+		--print("At the end of the string.")
+		return nil, nil
+	end
+	if (limit > #str) then --Width is too long for the string.
+		--print("Width oversteps the end of the string.")
+		limit = #str
+	end
+	pos, _ = str:find("\n", start)
+	if (pos ~= nil) then --there is a new line
+		--print("new line detected in string.")
+		if (pos <= limit) then --the \n is in range
+			--print("new line is within line width")
+			string = str:sub(start, pos)
+			nextchar = pos + 1
+			return string, nextchar
+		end
+	end
+	pos, _ = str:find("%s", start)
+	if (pos ~= nil) then --There are spaces
+		--print("spaces detected in string.")
+		if (pos <= limit) then --The space is in range
+			--print("spaces are within line width")
+			local curr = pos
+			while (true) do
+				pos, _ = str:find("%s", curr)
+				if (pos ~= nil) then --found another space
+					--print("space detected")
+					if (pos > limit) then
+						--print("space beyond limit")
+						string = str:sub(start, curr - 1)
+						nextchar = curr
+						break
+					else
+						--print("space within limit")
+						curr = pos + 1
+					end
+				else --there are no more spaces
+					--print("no more spaces detected")
+					if (limit == #str) then
+						string = str:sub(start, limit)
+						nextchar = limit + 1
+						break
+					else
+						string = str:sub(start, curr - 1)
+						nextchar = curr
+						break
+					end
+				end
 			end
-		else --have reached the end of the string
-			endpos = str:len() + 1 --misses out the last character
-			addnewline = false     --if I didn't +1 it.
-			break
+		else
+			--print("spaces are outside line width")
+			string = str:sub(start, limit)
+			nextchar = limit + 1
 		end
+	else --There are no spaces
+		string = str:sub(start, limit)
+		nextchar = limit + 1
 	end
-	endpos = endpos - 1 --str:find() points too far for str:sub
-	if endpos ~= nil then
-		string = str:sub(start, endpos)
-		if addnewline == true then
-			string = string .. "\n"
-		end
-	end
-
-	nextstart = endpos + 1
-	return string, nextstart
+	return string, nextchar
 end
 
 function util.splitstringintolines(str, width)
@@ -69,24 +102,13 @@ function util.splitstringintolines(str, width)
 	-- GOES INTO INFINITE LOOPS AT BAD TIMES. NEEDS FIXING
 	while(true) do
 		string, nextpos = util.getlinewithspaces(str, width, nextpos)
-		if string == "" or string == nil then
+		if string == nil then
 			break
 		end
-		print("In iteration " ..it..", string was: "..string)
+		--print("In iteration " ..it..", string was: "..string)
 		it = it + 1
 		table.insert(temp, string)
 		string = nil
-	end
-	--]]
-	--[[
-	while (nextpos < #str) do
-		if (nextpos + width < #str) then
-			string = str:sub(nextpos, nextpos + width)
-		else
-			string = str:sub(nextpos, #str)
-		end
-		table.insert(temp, string)
-		nextpos = nextpos + width
 	end
 	--]]
 	return temp
--- userlib/widget_textbox.lua
+++ userlib/widget_textbox.lua
@@ -5,9 +5,12 @@ local util = require "userlib/util"
 local w_textbox = {
 	draw = function(self, dt)
 		local offset = 0
+		local i
+		local startline = self.currentline
+		local endline = startline + self.linestodraw - 1
 		love.graphics.setColor(self.colour)
-		for i,v in ipairs(self.lines) do
-			love.graphics.print(v, self:getScreenX(),
+		for i = startline, endline do
+			love.graphics.print(self.lines[i], self:getScreenX(),
 			                    self:getScreenY() + offset)
 			offset = offset + game.ui.smallfont:getHeight()
 		end
@@ -17,18 +20,63 @@ local w_textbox = {
 			self.width = self.parent.width - 32
 		end
 	end,
+	testpoint = function(self, x, y)
+		if x >= self.posx + self.parent:getX()
+			and x <= self.posx + self.parent:getX() + self.width
+			and y >= self.posy + self.parent:getY()
+			and y <= self.posy + self.parent:getY() + self.height
+		then
+			return true
+		else
+			return false
+		end
+	end,
+	mousereleased = function(self, x, y, button)
+		print("click detected")
+		if (button == "wu") then
+			if (self.currentline > 1) then
+				self.currentline = self.currentline - 1
+			end
+		elseif (button == "wd") then
+			if (self.currentline < self.totallines - self.linestodraw + 1) then
+				self.currentline = self.currentline + 1
+			end
+		end
+	end,
+	makelines = function(self, text)
+		if text then
+			self.text = text
+		end
+		local charwidth = math.floor(self.width / 
+		                             game.ui.smallfont:getWidth("O"))
+		self.lines = util.splitstringintolines(self.text, charwidth)
+		self.totallines = #self.lines
+		self.linestodraw = math.floor(self.height / 
+		                              game.ui.smallfont:getHeight())
+		if (self.linestodraw > self.totallines) then
+			self.linestodraw = self.totallines - 1
+		end
+		if (self.currentline > self.totallines - self.linestodraw) then
+			self.currentline = self.totallines - self.linestodraw
+		end
+	end,
 	lines = nil,
+	currentline = nil,
+	totallines = nil,
+	linestodraw = nil,
 
 }
 w_textbox.new = function(data)
 
 	local temp = w_label.newlabel(data)
 	temp.update = w_textbox.update
-	
-	local charwidth = math.floor(temp.width/game.ui.smallfont:getWidth("O"))
-	print("One character of this font is "..game.ui.smallfont:getWidth("O")..". The textbox is ".. temp.width .. " pixels wide. "..charwidth.." characters will fit in this box")
-	temp.lines = util.splitstringintolines(temp.text, charwidth)
 	temp.draw = w_textbox.draw
+	temp.makelines = w_textbox.makelines
+	temp.mousereleased = w_textbox.mousereleased
+	temp.testpoint = w_textbox.testpoint
+	temp.currentline = 1
+	temp:makelines()
+
 	return temp
 end
 
